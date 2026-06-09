d55bb1e88619202fbb9977a94dfabd8b4cbe44d7
Lucentis <lucentis.dev@gmail.com>
2026-06-09 08:27:25 +0000

    resources/children: made relation managers

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -20,9 +20,10 @@ class UsersTable
         return $table
             ->columns([
                 TextColumn::make('name')
+                    ->label('Nom')
                     ->searchable(),
                 TextColumn::make('email')
-                    ->label('Email address')
+                    ->label('Adresse email')
                     ->searchable(),
                 TextColumn::make('roles.name')
                     ->label('Role')
@@ -35,18 +36,22 @@ class UsersTable
                         default => 'gray',
                     }),
                 TextColumn::make('email_verified_at')
+                    ->label('Email vérifié le:')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('created_at')
+                    ->label('Crée le:')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('updated_at')
+                    ->label('Mis à jour le:')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('deleted_at')
+                    ->label('Supprimé le:')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
